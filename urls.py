c9979e945061f09ce41c2e399106e79bd762cd1c
sagnik <sagnik1987@gmail.com>
2016-02-08 15:16:34 -0500

    interface for lucidworks fusion: ist 441

---
+++ urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+#import settings
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+     #url(r'^rsc/', include('rsc.urls', namespace="rsc")),
+     url(r'^$', 'rsc.views.home'),
+     #url(r''
+     url(r'^viewfigure/', 'rsc.views.viewfigure'),
+     url(r'^download/(?P<loc>.+)/$', 'rsc.views.download'),
+     #url(r'^download/(?P<loc>~[^0-9A-Za-z\\/]~)/$', 'rsc.views.download'),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+)
+
+
